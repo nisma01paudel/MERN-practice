2cdd8cd8ea772b31fde914f1533a575bc5c649a9
unknown <npaudel048@gmail.com>
2024-10-21 23:13:13 +0545

    learning node js

---
+++ learnphase/script.js
@@ -0,0 +1,14 @@
+//Variables take more storage
+const name1 ="Manish"
+const ageOfName1 =21;
+const professionOfName1 = "student"
+
+//so replace with OBJECTS
+const Person = {
+    name: "Manish",
+    age : 21,
+    profession: "student"
+}
+console.log(Person.name)
+console.log(Person.age)
+console.log(Person.profession)
---
+++ nodejs/app.js
@@ -0,0 +1,22 @@
+const a = 10;
+const b = 20;
+// console.log(a)
+// console.log(b)
+const name = "Manish"
+const sum = a + b
+console.log(sum)
+// console.log("Welcome to nodejs")
+
+//Array
+const numbers = [10,20,30]
+// console.log(numbers[1])
+//  to calculate length of array
+// console.log(numbers.length)
+
+// objects
+const Person ={
+    name : "Nisa",
+    age: 21
+}
+console.log("This is person name :" + Person.name)
+console.log(Person.age)
