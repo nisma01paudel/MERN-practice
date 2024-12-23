5bda65df9d804867bc14aeb6f5efc342e1a3991f
unknown <npaudel048@gmail.com>
2024-12-23 23:05:42 +0545

    Mernlearn

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,22 +1,28 @@
-const a = 10;
-const b = 20;
-// console.log(a)
-// console.log(b)
-const name = "Manish"
-const sum = a + b
-console.log(sum)
-// console.log("Welcome to nodejs")
+// const a = 10;
+// const b = 20;
+// // console.log(a)
+// // console.log(b)
+// const name = "Manish"
+// const sum = a + b
+// console.log(sum)
+// // console.log("Welcome to nodejs")
 
-//Array
-const numbers = [10,20,30]
-// console.log(numbers[1])
-//  to calculate length of array
-// console.log(numbers.length)
+// //Array
+// const numbers = [10,20,30]
+// // console.log(numbers[1])
+// //  to calculate length of array
+// // console.log(numbers.length)
 
-// objects
-const Person ={
-    name : "Nisa",
-    age: 21
-}
-console.log("This is person name :" + Person.name)
-console.log(Person.age)
+// // objects
+// const Person ={
+//     name : "Nisa",
+//     age: 21
+// }
+// console.log("This is person name :" + Person.name)
+// console.log(Person.age)
+
+const app = require("express")()
+
+//Alternate
+//const express = requires("express")
+//const app = express()
